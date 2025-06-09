41ab63baa0ac84477ca8c531c574464a4964393d
Priyanshucs22 <priyanshusharma0156@gmail.com>
2025-06-09 09:14:44 +0530

    first commit

---
+++ prime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class prime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        scanner.close();
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
